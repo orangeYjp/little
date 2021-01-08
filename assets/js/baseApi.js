d10011abdf828bb4e6954297e3ffb3192ff4aa1d
tangerine <906964212@qq.com>
2021-01-08 11:36:05 +0800

    提交login

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,20 @@
+// 因为每次都需要输入很长的url地址,因此将url端口之前的内容进行封装
+// $.ajaxPrefillter(function (options) {
+//     options.url = "http://api-breakingnews-web.itheima.net" + options.url
+//     // 有一部分请求需要有权限才可以发送,接口中常带有/my/
+//     if (options.url.indexOf('/my/')!== -1) {
+//         // 若options.url.indexOf('/my/'),则说明含有/my/ 请求需要权限,需要设定header属性
+//         options.headers = {
+//             Authorization: localStorage.getItem('token') || ''
+//         }
+//     }
+//     // 另外为了确保用户安全,每次登录前都需要进行身份认证
+//     options.complete =function(res){
+//         console.log(res);
+//     }
+// })
+
+
 // 因为每次ajax/post/get方法都需要提供很长的域名,因此封装此方法
 $.ajaxPrefilter(function(options){
     options.url ='http://api-breakingnews-web.itheima.net'+options.url
@@ -19,4 +36,3 @@ $.ajaxPrefilter(function(options){
     }
 // jquery  的ajax的回调函数一共有三种， 分别是success（成功时调用）/error（失败时调用）/complete（只要请求就会调用）
 }) 
-
